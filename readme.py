d9a9f51c222cbf86cf7c66e5572f4d1b7788bfb2
Ge Yang <yangge1987@gmail.com>
2021-03-19 12:57:28 -0400

    initial commit

---
+++ readme.py
@@ -0,0 +1,14 @@
+import blotch
+from cmx import doc
+from ml_logger import logger
+
+
+doc @ """
+# Example Time Series Analysis
+"""
+
+# files = blotch.glob("example_data/**/*.jsonl")
+# dfs = blotch.read_as_dataframes(files)
+#
+# series = dfs.map(bin, by="xKey").map("mean 25% 75%")
+
